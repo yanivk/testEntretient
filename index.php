91cbf06a68caf149300c2203d34b53e80bc0f676
yanivk <yanivk@users.noreply.github.com>
2020-09-10 14:38:45 +0200

    index test julien

---
+++ index.php
@@ -0,0 +1,56 @@
+class Product
+{
+//recupere le prix HT d’un produit
+public function getPriceWithoutTax(): ?float
+{
+    return $this->priceWithoutTax;
+}
+//Modifie le prix HT d’un produit
+public function setPriceWithoutTax(float $priceWithoutTax): self
+{
+    $this->priceWithoutTax = $priceWithoutTax;
+
+    return $this;
+}
+}
+*************************Class client**********************************
+class Client
+{
+//recupere le statut d’un client
+public function getStatus(): ?string
+{
+    return $this->status;
+}
+//Modifie le statut d’un client
+public function setStatus(string $status): self
+{
+    $this->status = $stauts;
+
+    return $this;
+}
+
+//recupere le produit d’un client
+public function getProduct(string $produt): ?string
+{
+    $this->product = $product;
+
+    return $this;
+}
+
+}
+
+/************************Connection BDD******************************/
+Public function getAllClients(Client $client){
+	*
+	*
+	*
+	//allClients est un array contenant tous les clients
+	Return $allClients;
+}
+
+
+
+**********************Base de travail********************************
+class changePriceProduct{
+	//ton code ici :D bon courage
+}
